5ec20414e8f4bfb1360ba43932b92822f07a949b
Caio Romano <caioromano13@gmail.com>
2023-12-16 04:02:41 -0300

    16.12.23 - setup theme

--- projeto_calculadora/main.py
+++ projeto_calculadora/main.py
@@ -4,15 +4,21 @@ from main_window import MainWindow
 from display import Display
 from PySide6.QtWidgets import QApplication
 from PySide6.QtGui import QIcon
+from info import Info
 from variables import ICON_PATH
+from styles import setupTheme
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    setupTheme()
     window = MainWindow()
     icon = QIcon(str(ICON_PATH))
 
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
+
+    info = Info('Label')
+    window.addWidgetToLayout(info)
 
     display = Display()
     display.setPlaceholderText('0')
